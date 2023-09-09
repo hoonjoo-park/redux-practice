a7eeec2ea499a70b46bfc76260d11e2144d0d21e
hoonjoo94 <hoonjoo94@naver.com>
2023-09-09 17:16:36 +0900

    App.tsx 위치 변경

---
+++ src/app/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Provider} from 'react-redux';
+import store, {persistor} from '../configs/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
