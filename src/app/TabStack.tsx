62ee9dba6cf5893f9fe6e1af4be5ee532f6a566b
hoonjoo94 <hoonjoo94@naver.com>
2023-09-13 01:09:00 +0900

    tabBarLabel 수정

--- src/app/TabStack.tsx
+++ src/app/TabStack.tsx
@@ -3,14 +3,28 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Result from '../screens/Result';
 import MyProfile from '../screens/MyProfile';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 const TabStack = () => {
+  const getTabBarLabel = (route: any) => {
+    switch (route.name) {
+      case 'HomeTab':
+        return <Text>홈</Text>;
+      case 'Result':
+        return <Text>결과</Text>;
+      case 'MyProfile':
+        return <Text>프로필</Text>;
+    }
+  };
   return (
     <Tab.Navigator
       initialRouteName="HomeTab"
-      screenOptions={{headerShown: false}}>
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarLabel: () => getTabBarLabel(route),
+      })}>
       <Tab.Screen name="HomeTab" component={Home} />
       <Tab.Screen name="Result" component={Result} />
       <Tab.Screen name="MyProfile" component={MyProfile} />
