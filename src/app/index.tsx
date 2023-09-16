ef08801a3ae2ca4c9f5b7ecd6ba59599ccadf654
hoonjoo94 <hoonjoo94@naver.com>
2023-09-16 17:33:38 +0900

    statusBar dark-content로 고정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
-import store, {persistor} from '../configs/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import store, {persistor} from '../configs/store';
 import RootStack from './RootStack';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
+        <StatusBar barStyle={'dark-content'} backgroundColor="#fff" />
         <RootStack />
       </PersistGate>
     </Provider>
